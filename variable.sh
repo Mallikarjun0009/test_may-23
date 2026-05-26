3eff14c6402736dbb1efce12d50d9a7c32f4030e
unknown <amalli101010@gmail.com>
2026-05-26 10:47:24 +0530

    2nd commint

--- variable.sh
+++ variable.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-PERSON1=Robert
-PERSON2=Raheem
+PERSON1=Kutty
+PERSON2=Tinkku
 
-echo "$PERSON1: Hello $PERSON2, Good Morning"
-echo "$PERSON2: Hi $PERSON1, very Good Morning"
-echo "$PERSON1: How are you $PERSON2?"
-echo "$PERSON2: I am good $PERSON1, How are you"
+echo"$PERSON1 :: Hello $PERSON2, Good Morning"
+echo"$PERSON2 :: Hi $PERSON1, Very good morning"
+
+
 
 
 
