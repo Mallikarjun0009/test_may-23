a6a30a9592199a595078475a16db92c4e5a05311
unknown <amalli101010@gmail.com>
2026-05-26 10:58:45 +0530

    1nd commint

---
+++ variable02.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "please enter your username:"
+
+read -s USERNAME
+
+echo "Please endtr your password:"
+
+read -s PASSWORD
+
+echo "the username is $USERNAME, the password is $PASSWORD"
