df114c2f4ca7741732f12b909e3be9cfc9a233be
unknown <amalli101010@gmail.com>
2026-05-26 11:23:22 +0530

    1nd commint

---
+++ arrys.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Mango" "Greaps")
+
+echo "1st value : ${FRUITS[0]}"
+echo "2nd value : ${FRUITS[1]}"
+echo "3rd Value : ${FRUITS[2]}"
+echo "4th Value : ${FRUITS[3]}"
+
+echo "show me the all value here: $@"
