4b7c1ee88ce9a230faf7c78ccb787de1232ac1bf
unknown <amalli101010@gmail.com>
2026-05-26 12:15:07 +0530

    1nd commint

---
+++ install-mysql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ID=$(id -u)
+
+if [$ID -ne 0]
+then
+    echo "Error: Kndly run this script into a root user"
+    exit 1
+else
+    echo "Yes..!! your in root user"
+fi 
+
+yum install mysql -y
+
+if [ $? -ne 0]
+then 
+    echo "Error : installing mysql is faild"
+else
+    echo "instalation is ... success..!!"
+fi
