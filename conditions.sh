c23013a79327ba925e48e590926e027a18a7e0b2
unknown <amalli101010@gmail.com>
2026-05-26 11:55:37 +0530

    1nd commint

--- conditions.sh
+++ conditions.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Check if an argument was provided
+if [ -z "$1" ]; then
+   echo "Error: Please provide a number."
+   echo "Usage: $0 <number>"
+   exit 1
+fi
+
 NUMBER=$1
 
-if [ $NUMBER -gt 100 ]
-then
-   echo "Give number $NUMBER is greater than 100"
+# Compare the number
+if [ "$NUMBER" -gt 100 ]; then
+   echo "Given number $NUMBER is greater than 100"
 else
-   echo "Give number $NUMBER is not greater than 100"
+   echo "Given number $NUMBER is not greater than 100"
 fi
