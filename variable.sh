9095ce8c2741c64ee584da07574c19b4ca68da60
unknown <amalli101010@gmail.com>
2026-05-26 10:53:42 +0530

    2nd commint

--- variable.sh
+++ variable.sh
@@ -9,5 +9,10 @@ echo "$PERSON2 : Hi $PERSON1, Very good morning"
 date=$(date)
 echo "starting the exatly: $(date)"
 
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : Hello $PERSON2, Good Morning"
+echo "$PERSON2 : Hi $PERSON1, Very good morning"
 
 
